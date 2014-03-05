71d482263b9f590e22dda2ee43e2df72f02815f3
lockgo <juan.t.chen@gmail.com>
2014-03-05 00:16:59 -0500

    Test
    test

---
+++ diffie/Program.py
@@ -0,0 +1,26 @@
+﻿#import urllib
+
+def diffie(a, p, g, b):
+	Alice = (g**a) % p 
+	Bob = (g**b) % p
+	s = (Bob**a) % p
+	t = (Alice**b) % p
+	print "Alice is %d!\n" 	%Alice
+	#print "g to the a is %d\n"  %testout
+	print "and Bob is %d\n"	%Bob
+	print "and s is %d\n"  	%s
+	print "and t is %d"	%t
+
+inputing = raw_input("please input a\n")
+a = int(inputing)
+inputing = raw_input("please input p\n")
+p = int(inputing)
+inputing = raw_input("please input g\n")
+g = int(inputing)
+inputing = raw_input("please input b\n")
+b = int(inputing)
+diffie(a, p, g, b);
+
+inputing = raw_input("please input b\n")
+itsover = int(inputing)
+
